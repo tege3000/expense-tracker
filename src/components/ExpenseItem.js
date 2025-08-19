89e2d82e45ec4644b187454392100bae80d701ac
Tito Egeonu <teetoegeonu@gmail.com>
2025-08-19 16:09:21 +0100

    Add - Added data for expensis to dynamically populate expenses list

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,24 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const date = new Date(props.date);
+    const month = date.toLocaleString('en-US', { month: 'long' });  
+    const day = date.toLocaleString('en-US', { day: '2-digit' });
+    const year = date.getFullYear();    
+    const price = props.price.toFixed(2);       
+
     return (
         <div className='container'>
             <div className='tracker-left'>
-                <h4 className='date-month'>August</h4>
-                <h6 className='date-year'>2020</h6>
-                <h2 className='date-day'>14</h2>
+                <h4 className='date-month'>{month}</h4>
+                <h6 className='date-year'>{year}</h6>
+                <h2 className='date-day'>{day}</h2>
             </div>
             <div className='tracker-center'>
-                <h2>Toilet Paper</h2>
+                <h2>{props.item}</h2>
             </div>
             <div className='tracker-right'>
-                <h2 className='price'>$94.12</h2>
+                <h2 className='price'>${price}</h2>
             </div>
         </div>
     );
